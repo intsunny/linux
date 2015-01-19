920529f2e11c7339aa072aa5e67199c895999682
Sunny Dubey <sunny@audiowiki.org>
2015-01-19 12:11:01 -0500

    Add manual configuration

---
+++ mackbookpro11,1/touchpad.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setup rpmfusion, install the needed package, build and load the module
+
+PATH=/bin:/usr/bin
+
+synclient TapButton1=1
+synclient TapButton2=2
+synclient TapButton3=3
+synclient VertEdgeScroll=1
+synclient VertTwoFingerScroll=1
+synclient HorizEdgeScroll=1
+synclient HorizTwoFingerScroll=1
+synclient CircularScrolling=0
+synclient PalmDetect=1
+synclient EmulateTwoFingerMinZ=40
+synclient EmulateTwoFingerMinW=8
+synclient CoastingSpeed=0
+synclient FingerLow=35
+synclient FingerHigh=40
+synclient VertScrollDelta=-111
+synclient HorizScrollDelta=-111
